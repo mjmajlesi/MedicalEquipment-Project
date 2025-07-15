98636d1594ee30b039e1cd78aee675e4022dac57
محمد جواد مجلسی <mohammadjavadmajlesi313@gmail.com>
2025-07-16 02:47:45 +0330

    create navbar component and improve that.

--- FrontEnd/app/_Components/Navbar.tsx
+++ FrontEnd/app/_Components/Navbar.tsx
@@ -1,8 +1,45 @@
+"use client"
+import Link from 'next/link'
 import React from 'react'
+import Button from './buttuns'
+import { usePathname } from 'next/navigation'
 
 function Navbar() {
+  const navs = [
+    {
+      name: "خانه",
+      link: "/"
+    },
+    {
+      name: "محصولات",
+      link: "/products"
+    },
+    {
+      name: "تماس با ما",
+      link: "/contects"
+    },
+  ]
+  const pathname = usePathname();
+
   return (
-    <div>Navbar</div>
+    <div className='px-6 pb-4 my-4 flex justify-between items-center'>
+      <div >
+        <h1 className='text-2xl font-bold'>لگو</h1>
+      </div>
+      <div className='flex flex-row items-center gap-6'>
+        {navs.map(nav => (
+          <Link className={` ${pathname === nav.link ? "text-[#0d994d]" : "text-[#ededed]"} 
+            relative transition-all duration-300 ease-in-out hover:text-[18px]
+            after:content-[''] after:absolute after:left-0 after:-bottom-1 after:w-0 hover:after:w-full after:h-[2px] after:bg-[#ededed] after:transition-all after:duration-300
+            font-semibold`}
+            key={nav.name} href={nav.link}>{nav.name}
+          </Link>
+        ))}
+      </div>
+      <div>
+        <Button variant='login'>Login</Button>
+      </div>
+    </div>
   )
 }
 
--- FrontEnd/app/layout.tsx
+++ FrontEnd/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Vazirmatn } from "next/font/google";
 import localFont from "next/font/local"
 import "./globals.css";
+import Navbar from "./_Components/Navbar";
 
 const vazirmatn = Vazirmatn({
   variable: "--font-main",
@@ -31,6 +32,7 @@ export default function RootLayout({
       <body
         className={`${vazirmatn.className} antialiased`}
       >
+        <Navbar />
         {children}
       </body>
     </html>
