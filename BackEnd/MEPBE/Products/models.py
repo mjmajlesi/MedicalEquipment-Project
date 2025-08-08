2e649ed6498232ea188cdbf70a4976ccdc04e928
Mamaarsh <aj.silent.84@gmail.com>
2025-08-08 16:21:39 +0330

    edit image url for show in host and move all image file in image folder Version 0.3

--- BackEnd/MEPBE/Products/models.py
+++ BackEnd/MEPBE/Products/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=255, unique=True)
     slug = models.SlugField(unique=True, blank=True)
-    image = models.ImageField(upload_to='products/images/', blank=True, null=True)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
