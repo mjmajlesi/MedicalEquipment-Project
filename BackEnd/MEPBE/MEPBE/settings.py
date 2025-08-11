bcbef2bd727131ef2994a1f6ff5ce82d6c1d2ae7
Mamaarsh <aj.silent.84@gmail.com>
2025-08-11 23:51:03 +0330

    add pos\

--- BackEnd/MEPBE/MEPBE/settings.py
+++ BackEnd/MEPBE/MEPBE/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -83,10 +84,11 @@ WSGI_APPLICATION = "MEPBE.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASURL'),
+        conn_max_age=600, 
+        ssl_require=True   
+    )
 }
 
 
