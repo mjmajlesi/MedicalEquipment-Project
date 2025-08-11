b74efaa9fef9afd89fabf6a52fa64c6427d8f047
محمد جواد مجلسی <mohammadjavadmajlesi313@gmail.com>
2025-08-11 14:28:24 +0330

    add gh-pages to project for deploy to github

--- FrontEnd/next.config.ts
+++ FrontEnd/next.config.ts
@@ -2,9 +2,13 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    domains: ['medicalequipment-project.onrender.com'],
+    unoptimized: true, // چون GH Pages image optimizer نداره
   },
-}
+  basePath: '/MedicalEquipment-Project', // اسم ریپوی گیت‌هاب
+  assetPrefix: '/MedicalEquipment-Project/',
+};
 
 module.exports = nextConfig;
+
