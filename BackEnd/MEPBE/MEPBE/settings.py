66013e93aabccff2c153ed2e0f1450fef25043f4
Mamaarsh <aj.silent.84@gmail.com>
2026-07-30 14:20:45 +0330

    create Dockerfile for BackEnd & make change in MIDDLEWARE section in settings.py file & edit requirements.txt file

--- BackEnd/MEPBE/MEPBE/settings.py
+++ BackEnd/MEPBE/MEPBE/settings.py
@@ -52,10 +52,10 @@ INSTALLED_APPS = [
 # MIDDLEWARE
 # ====================
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
-
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
